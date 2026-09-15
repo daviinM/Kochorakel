2fea4fa5450e479c23473d0daff2ea1c1554c8a3
daviinM <davinmerkel77@gmail.com>
2026-09-15 09:44:15 +0200

    App-Cache auf Version 10 aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kochorakel-v9";
+const CACHE_NAME = "kochorakel-v10";
 const APP_SHELL = [
   "./", "./index.html", "./kochorakel.html", "./manifest.webmanifest",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png"
