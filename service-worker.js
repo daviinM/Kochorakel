adfa5fe4392c971383728dd6eac6b6da4b2cc429
daviinM <davinmerkel77@gmail.com>
2026-09-15 07:34:23 +0200

    Rezeptansicht und Warenkorb verbessern

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kochorakel-v2";
+const CACHE_NAME = "kochorakel-v3";
 const APP_SHELL = [
   "./", "./index.html", "./kochorakel.html", "./manifest.webmanifest",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png"
