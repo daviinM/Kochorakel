f3f3fb14469633ec43cc651d04235b37060a5b23
daviinM <davinmerkel77@gmail.com>
2026-09-15 07:52:24 +0200

    Refresh cache for recipe sheet style fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kochorakel-v5";
+const CACHE_NAME = "kochorakel-v6";
 const APP_SHELL = [
   "./", "./index.html", "./kochorakel.html", "./manifest.webmanifest",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png"
